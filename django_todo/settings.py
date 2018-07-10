0046c280cf0c839a7566de5b490b96f9141e24a7
Gilly <gillianohea@gmail.com>
2018-07-10 12:41:55 +0000

    update setting enviro vars

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), os.environ.get('HOSTNAME')]
 
+host = os.environ.get('SITE_HOST')
+if host:
+    ALLOWED_HOSTS.append(host)
+
 # Application definition
 
 INSTALLED_APPS = [
